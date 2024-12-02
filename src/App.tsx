d8820dbbb7a90b2720a446d977012c0105a073d2
Joe Young <musicjoeyoung@gmail.com>
2024-12-02 14:19:33 -0500

    restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import './App.css'
+import "./App.scss"
 
-import { useEffect, useState } from 'react'
+import GooseFacts from "./components/GooseFacts/GooseFacts"
 
-import axios from 'axios'
-
-function App() {
-  const [geese, setGeese] = useState([])
-  const [randomFact, setRandomFact] = useState<any>(null)
-
-  const displayRandomFact = () => {
-    const randomIndex = Math.floor(Math.random() * geese.length)
-    setRandomFact(geese[randomIndex])
-  }
-
-  const getData = async () => {
-    try {
-      const response = await axios.get("http://localhost:8787/api/geese-trivia")
-      console.log(response.data.geeseTrivia)
-      setGeese(response.data.geeseTrivia)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-
+const App = () => {
   return (
-    <div>
-      <h1>Geese Trivia</h1>
-
-      <button onClick={displayRandomFact}>Show Random Fact</button>
-      {randomFact && (
-        <div>
-          <h2>{randomFact.question}</h2>
-          <p>{randomFact.answer}</p>
-        </div>
-      )}
-    </div>
+    <>
+      <GooseFacts />
+    </>
   )
 }
-
 export default App

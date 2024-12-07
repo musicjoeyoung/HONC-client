464871ff39aa8cc2551ed51d7dabe66752424078
Joe Young <musicjoeyoung@gmail.com>
2024-12-07 13:31:58 -0500

    single image finally rendered

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.scss"
 
 import GooseBot from "./components/GooseBot/GooseBot"
 import GooseFacts from "./components/GooseFacts/GooseFacts"
+import GooseImages from "./components/GooseImages/GooseImages"
 
 const App = () => {
   return (
     <>
       <GooseFacts />
       <GooseBot />
+      <GooseImages />
     </>
   )
 }

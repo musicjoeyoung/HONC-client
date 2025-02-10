a930f02f36417bc6f5b9c658eb2039d1e8733cf7
Joe Young <musicjoeyoung@gmail.com>
2025-02-10 12:30:22 -0500

    link in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import "./Footer.scss"
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© Spooking Honc - 2024 Joseph Young</p>
+            <p>© Spooking Honc - 2024 <a href="https://josephmyoung.com/" target="_blank">Joseph Young</a></p>
             <a className="footer__link" href="https://honc.dev/honcathon">Honcathon</a>
             <p className="footer__attribution">Goose Low Poly 3D Model | Created by Atlasmodels | Available at <a href="https://sketchfab.com/">Sketchfab </a> | Licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a></p>
         </footer>
